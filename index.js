0a04791f2e2f65c0ead3c19aad832e71d7c7c788
Daniel Small <piktur.io@gmail.com>
2018-10-16 00:49:13 +1100

    ⚡️ improvement: implement runner utilises WebdriverIO CLI rather than launcher

---
+++ index.js
@@ -0,0 +1,117 @@
+module.exports = (api, options) => {
+  api.registerCommand('test:e2e', {
+    description: 'run e2e tests with webdriverio',
+    usage: 'vue-cli-service test:e2e [options]',
+    options: {
+      '-b, --baseUrl': 'run e2e tests against given url instead of auto-starting dev server',
+      '--config': 'use custom webdriver config file (overrides internals)',
+      '--headless': 'run e2e tests in headless mode without GUI',
+      '--capabilities': 'specify comma-delimited browser capabilities to run in (default: phablet,mobile)',
+    },
+    details:
+      `All WebdriverIO CLI options are also supported.\n` +
+      `http://webdriver.io/guide/testrunner/gettingstarted.html`
+  }, async (args, rawArgs) => {
+    const server = await wdioServer(rawArgs, api)
+    await wdioPort(rawArgs)
+    wdioCapabilities(rawArgs)
+    wdioMode(rawArgs)
+    wdioConfig(rawArgs, api.resolve('wdio.conf.js'))
+
+    const { execa } = require('@vue/cli-shared-utils')
+    const wdioBinPath = require.resolve('webdriverio/bin/wdio')
+    const runner = execa(wdioBinPath, rawArgs, { stdio: 'inherit' })
+
+    if (server) {
+      runner.on('exit', () => server.close())
+      runner.on('error', () => server.close())
+    }
+
+    if (process.env.VUE_CLI_TEST) {
+      runner.on('exit', code => process.exit(code))
+    }
+
+    // WDIO launcher returns exit code 1 on failure consequently execa throws,
+    // catch to suppress unnecessary stdout pollution.
+    return runner.catch(err => null)
+  })
+}
+
+module.exports.defaultModes = {
+  'test:e2e': 'test'
+}
+
+async function wdioServer(rawArgs, api) {
+  const baseUrlPos = rawArgs.indexOf('--baseUrl')
+  const serverPromise = baseUrlPos === -1
+    ? api.service.run('serve')
+    : Promise.resolve({ url: rawArgs.splice(baseUrlPos, 2)[1] })
+
+  const { server, url } = await serverPromise
+
+  rawArgs.push('--baseUrl', url)
+
+  return server
+}
+
+async function wdioPort(rawArgs) {
+  const getPort = require('get-port')
+
+  if (rawArgs.indexOf('--port') === -1) {
+    rawArgs.push('--port', await getPort()) // find available port
+  }
+}
+
+function wdioMode(rawArgs) {
+  const headlessPos = rawArgs.indexOf('--headless')
+  if (headlessPos === -1) {
+    process.env.DEBUG = '1'
+  } else {
+    rawArgs.splice(headlessPos, 1)
+  }
+}
+
+function wdioCapabilities(rawArgs) {
+  const capabilitiesPos = rawArgs.indexOf('--capabilities')
+  if (capabilitiesPos === -1) {
+    process.env.WDIO_CAPABILITIES = 'phablet,mobile'
+  } else {
+    process.env.WDIO_CAPABILITIES = rawArgs.splice(capabilitiesPos, 2)
+  }
+
+  if (process.env.MOBILE_ONLY) {
+    process.env.WDIO_CAPABILITIES = 'mobile'
+  } else if (process.env.PHABLET_ONLY) {
+    process.env.WDIO_CAPABILITIES = 'phablet'
+  }
+}
+
+function wdioConfig(rawArgs, overridePath) {
+  const fs = require('fs')
+  const path = require('path')
+
+  const defaultPath = path.resolve(__dirname, './wdio.conf.debug.js')
+  let configPath
+
+  const configPos = rawArgs.indexOf('--config')
+  if (configPos !== -1) {
+    configPath = rawArgs.splice(configPos, 2)[1]
+  }
+
+  rawArgs.push(fs.existsSync(configPath) ? configPath : defaultPath)
+
+  if (fs.existsSync(overridePath)) {
+    // expose user overrides to config file
+    process.env.WDIO_CONFIG_OVERRIDE_PATH = overridePath
+  }
+}
+
+function removeArg (rawArgs, arg, offset = 1) {
+  const matchRE = new RegExp(`^--${arg}`)
+  const equalRE = new RegExp(`^--${arg}=`)
+  const i = rawArgs.findIndex(arg => matchRE.test(arg))
+
+  if (i > -1) {
+    rawArgs.splice(i, offset + (equalRE.test(rawArgs[i]) ? 0 : 1))
+  }
+}
