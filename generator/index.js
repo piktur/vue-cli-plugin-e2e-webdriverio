69e7f9b9e579f8b5bbb9c704eecacb96ffdb1046
Daniel Small <piktur.io@gmail.com>
2018-10-19 10:45:09 +1100

    👕 refactor: install dependencies conditionally, extend eslint is plugin used

--- generator/index.js
+++ generator/index.js
@@ -1,7 +1,56 @@
-module.exports = api => {
+const { PLUGIN_NAME } = require('../lib/constants')
+
+module.exports = (api, options) => {
+  (options.config === '') && (options.config = undefined)
+
+  // Don't laden the host project with erroneous dependencies. If the user prefers their
+  // own configuration, they can keep their configuration. In this case we make no assumptions
+  // about the testing framework, assertions/mocking libraries and WebdriverIO plugins; the user
+  // is responsibile for dependency management. Suggest they use WebdriverIO's configurator for
+  // this purpose.
+  if (!options.config) {
+    api.extendPackage({
+      devDependencies: {
+        'chromedriver': '^2.42.0',
+        'wdio-chromedriver-service': '^0.1.3',
+        'wdio-mocha-framework': '^0.6.2',
+        'wdio-spec-reporter': '^0.1.5',
+      }
+    })
+
+    api.hasPlugin('mocha') || api.extendPackage({
+      devDependencies: {
+        'chai': '^4.1.2',
+        'sinon': '^6.1.4',
+        'sinon-chai': '^3.2.0',
+      },
+    })
+
+    api.hasPlugin('eslint') && api.extendPackage({
+      eslintConfig: {
+        env: {
+          mocha: true,
+        },
+      },
+    })
+  }
+
   api.extendPackage({
     scripts: {
-      "test:e2e": "vue-cli-service test:e2e"
+      'test:e2e': 'vue-cli-service test:e2e'
+    },
+    vue: {
+      pluginOptions: {
+        [PLUGIN_NAME]: options,
+      }
+    }
+  })
+
+  api.hasPlugin('eslint') && api.extendPackage({
+    eslintConfig: {
+      globals: {
+        browser: true
+      }
     }
   })
 }
